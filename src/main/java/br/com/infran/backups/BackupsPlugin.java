1c5dabeadad585b7634d32f3202e27611de110bf
Lucas Infran <lucasinfrandepaulo8@gmail.com>
2021-05-21 12:11:13 -0400

    Backup Plugin

---
+++ src/main/java/br/com/infran/backups/BackupsPlugin.java
@@ -0,0 +1,37 @@
+package br.com.infran.backups;
+
+import br.com.infran.backups.runnable.TimedBackupTask;
+import br.com.infran.backups.service.BackupService;
+import io.github.eikefs.minecraft.lib.ConfigValue;
+import io.github.eikefs.minecraft.lib.api.ConfigLoader;
+import io.github.eikefs.minecraft.lib.api.mapper.ConfigMapper;
+import lombok.Getter;
+import me.bristermitten.pdm.PluginDependencyManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public final class BackupsPlugin extends JavaPlugin {
+
+    @Getter private BackupService backupService;
+
+    @Override
+    public void onEnable() {
+        PluginDependencyManager.of(this).loadAllDependencies().thenRun(() -> {
+            ConfigLoader loader = new ConfigLoader(getDataFolder());
+
+            loader.map(TimedBackupTask.class);
+
+            new TimedBackupTask(getBackupService(), this).runTaskTimer(this, TimedBackupTask.delay, 1200);
+        });
+
+        this.backupService = new BackupService(this);
+
+        saveDefaultConfig();
+
+        File backupDir = new File(getDataFolder(), "backups");
+
+        if (!backupDir.exists()) backupDir.mkdir();
+    }
+
+}
---
+++ src/main/java/br/com/infran/backups/runnable/TimedBackupTask.java
@@ -0,0 +1,57 @@
+package br.com.infran.backups.runnable;
+
+import br.com.infran.backups.compressor.ZipCompressor;
+import br.com.infran.backups.service.BackupService;
+import io.github.eikefs.minecraft.lib.ConfigValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.SneakyThrows;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.ZipOutputStream;
+
+@AllArgsConstructor
+@Getter
+public class TimedBackupTask extends BukkitRunnable {
+
+    @ConfigValue("ticks-delay") public static Integer delay;
+
+    private final BackupService service;
+    private final JavaPlugin plugin;
+
+    @Override
+    public void run() {
+        final Logger log = getPlugin().getLogger();
+
+        final File backupFile = service.getBackupFile();
+        final File outputFile = new File(plugin.getDataFolder() + "/backups",
+                Calendar.getInstance().toInstant().toString().replace(":", "-") + ".zip");
+
+        try (
+                FileOutputStream outputStream = new FileOutputStream(outputFile);
+                ZipOutputStream zipOutput = new ZipOutputStream(outputStream)) {
+
+            final long start = System.currentTimeMillis();
+
+            log.warning("Criando backup! O servidor pode ficar um pouco instável...");
+
+            // TODO: Async this
+            ZipCompressor.zip(backupFile, backupFile.getName(), zipOutput);
+
+            backupFile.delete();
+
+            log.info(String.format("Backup foi concluído! Time decorrido: %s seconds!", ((System.currentTimeMillis() - start) / 1000)));
+        } catch (Exception exception) {
+            log.log(Level.SEVERE, "Fatal: " + exception.getMessage());
+
+            cancel();
+        }
+    }
+
+}
